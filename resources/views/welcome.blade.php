a70fe626ac72f3a8f825b0487c7da03d0d03f3aa
verryp <verryp.dev@gmail.com>
2020-02-18 11:09:27 +0700

    feat(webpack): adding browser syn lib

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,33 +1,44 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
 
-    <title>Laravel</title>
+        <!-- Fonts -->
+        <link
+            href="https://fonts.googleapis.com/css?family=Nunito:200,600"
+            rel="stylesheet"
+        />
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+    </head>
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-</head>
-
-<body>
-    <div class="md:flex container border p-4">
-        <div class="md:flex-shrink-0">
-            <img class="rounded-lg md:w-56"
-                src="https://images.unsplash.com/photo-1556740738-b6a63e27c4df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=448&q=80"
-                alt="Woman paying for a purchase">
+    <body>
+        <div
+            class="flex container justify-center p-4 mx-auto content-center h-screen"
+        >
+            <div class="flex content-center align-center flex-wrap">
+                <div class="mt-4 md:mt-0 md:ml-6">
+                    <div
+                        class="uppercase tracking-wide text-sm text-indigo-600 font-bold"
+                    >
+                        {{ $platform }}
+                    </div>
+                    <a
+                        href="#"
+                        class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline"
+                    >
+                        {{ $version }}
+                    </a>
+                    <p class="mt-2 text-gray-600">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing
+                        elit. Eius consequatur natus dicta ipsa eos amet quam
+                        quis tempora sint odio. In minima veritatis magnam iusto
+                        iure facere totam repudiandae. Dolor?
+                    </p>
+                </div>
+            </div>
         </div>
-        <div class="mt-4 md:mt-0 md:ml-6">
-            <div class="uppercase tracking-wide text-sm text-indigo-600 font-bold">Marketing</div>
-            <a href="#" class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">Finding
-                customers for your new business</a>
-            <p class="mt-2 text-gray-600">Getting a new business off the ground is a lot of hard work. Here are five
-                ideas
-                you can use to find your first customers.</p>
-        </div>
-    </div>
-</body>
-
+    </body>
 </html>
